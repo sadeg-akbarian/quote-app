0f13fe127f66a6544b3a70a6754f3910863a2d93
sadeg-akbarian <sadeg.akbarian@gmail.com>
2023-10-28 13:51:33 +0200

    refactor: better state management, seperate function for rendering the state

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const button = document.querySelector("button");
 const main = document.querySelector("main");
 
+function renderState(xxx) {
+  main.innerHTML = "";
+  const quoteParagraph = document.createElement("p");
+  const quoteText = document.createTextNode(xxx.quote);
+  quoteParagraph.appendChild(quoteText);
+  main.appendChild(quoteParagraph);
+  const authorParagraph = document.createElement("p");
+  const authorName = document.createTextNode("-- " + xxx.author);
+  authorParagraph.appendChild(authorName);
+  main.appendChild(authorParagraph);
+}
+
 button.addEventListener("click", function () {
   fetch("https://dummy-apis.netlify.app/api/quote")
     .then((response) => {
@@ -9,14 +21,6 @@ button.addEventListener("click", function () {
       }
     })
     .then((data) => {
-      main.innerHTML = "";
-      const quoteParagraph = document.createElement("p");
-      const quoteText = document.createTextNode(data.quote);
-      quoteParagraph.appendChild(quoteText);
-      main.appendChild(quoteParagraph);
-      const authorParagraph = document.createElement("p");
-      const authorName = document.createTextNode("-- " + data.author);
-      authorParagraph.appendChild(authorName);
-      main.appendChild(authorParagraph);
+      renderState(data);
     });
 });
